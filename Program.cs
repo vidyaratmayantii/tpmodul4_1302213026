71d6cfd74f5f78fe9c57c2346deedcfcb2f7f999
LAPTOP-GTQCMDG9\Asus <vidyaratmayanti@gmail.com>
2023-03-11 15:19:06 +0700

    menambahkan implementasi dengan table-driven

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+public class AppDomainUnloadedException
+{
+    public enum Kelurahan {Batununggal, Kujangsari, Mengger, Wates, Cijaura, Jatisari, Margasari, Sekejari, Kebonwaru, Maleer, Samoja }
+    
+    public static int getKodePos(Kelurahan kelurahan)
+    {
+        int[] kode = { 40266, 40287, 40267, 40256, 40287, 40286, 40286, 40272, 40274, 40273 };
+        return kode[(int)kelurahan];
+    }
+
+    public static void Main(string[] args)
+    {
+        Kelurahan kelurahan = Kelurahan.Cijaura;
+        int kodepos = getKodePos(kelurahan);
+        Console.WriteLine("Kelurahan " + kelurahan + " kode pos 0" + kodepos);
+    }
+}
